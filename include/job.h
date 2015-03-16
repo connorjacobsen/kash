3236945b0f52a9b2abf8590c7ada988b4578fdbe
Connor Jacobsen <jacobsen.connor@gmail.com>
2015-03-16 15:20:50 -0400

    add job header

---
+++ include/job.h
@@ -0,0 +1,61 @@
+#ifndef JOB_H_
+#define JOB_H_
+
+#include <sys/types.h> /* need pid_t */
+
+/**
+ * A job is a pipeline of processes.
+ *
+ * struct job_t manages all the information about a currently
+ * executing job.
+ */
+typedef struct job_t {
+    int jid;                    /* the job ID */
+    struct job_t *next;         /* next active job */
+    char *command;              /* command line, used for messages */
+    process_t *first_process;   /* list of processes in this job */
+    pid_t pgid;                 /* process group ID */
+    int stdin, stdout, stderr;  /* standard i/o channels */
+} job_t;
+
+/* The active jobs are linked into a list. This is its head. */
+job_t *first_job = NULL;
+
+/* The jid to assign to the next created job. */
+int next_jid = 1;
+
+/* Initializes a new job_t object. */
+job_t *make_job(pid_t pgid, const char *command);
+
+/**
+ * Adds a job to the provided job list.
+ * Return 1 if the job was successfully added to the jobs list.
+ */
+int add_job(job_t *job_list, job_t *job);
+
+/* Find the active job with the indicated pgid. */
+job *find_job (pid_t pgid);
+
+/**
+ * Return 1 if the job with the provided pgid was successfully removed.
+ * If the job was not successfully removed or the pgid was bad, it will
+ * fail and return 0.
+ */
+int delete_job(job_t *job_list, pid_t pgid);
+
+/* Return 1 if all processes in the job have stopped or completed. */
+int job_is_stopped (job_t *job);
+
+/* Return 1 if all processes in the job have completed. */
+int job_is_completed (job_t *job);
+
+/* Return 1 if all jobs are successfully removed from the job_list */
+int clear_job_list(job_t *job_list);
+
+/* Return the pgid of the current foreground job, or 0 if no such job. */
+pid_t current_foreground_job(job_t* job_list);
+
+/* Return the jid associated with the given pgid, or 0 if no such pgid. */
+int jid_from_pgid(pid_t pgid);
+
+#endif /* JOB_H_ */
