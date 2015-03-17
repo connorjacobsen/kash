4ee08d52de1cbe3d69b41cc1ff33be6625c299c2
Connor Jacobsen <jacobsen.connor@gmail.com>
2015-03-16 23:12:08 -0400

    add comments

--- include/kash.h
+++ include/kash.h
@@ -12,9 +12,11 @@
 /* initializes the shell */
 void init_shell(void);
 
+/* launch a new process */
 void launch_process(process_t *p, pid_t pgid, int infile,
                     int outfile, int errfile, int foreground);
 
+/* launch a new job */
 void launch_job(job_t *job, int foreground);
 
 #endif /* KASH_H_ */
