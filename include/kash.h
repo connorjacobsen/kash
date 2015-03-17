81e439d73542c8cb30211589fe6a853e55a2d68e
Connor Jacobsen <jacobsen.connor@gmail.com>
2015-03-16 21:16:29 -0400

    update kash header

--- include/kash.h
+++ include/kash.h
@@ -1,6 +1,20 @@
 #ifndef KASH_H_
 #define KASH_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <unistd.h>
+#include "constants.h"
+#include "job.h"
+#include "process.h"
 
+/* initializes the shell */
+void init_shell(void);
 
+void launch_process(process_t *p, pid_t pgid, int infile,
+                    int outfile, int errfile, int foreground);
+
+void launch_job(job_t *job, int foreground);
 
 #endif /* KASH_H_ */
