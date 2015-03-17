e7d1358e82ab9e55696fc31fe11379f4c3da807e
Connor Jacobsen <jacobsen.connor@gmail.com>
2015-03-16 21:16:43 -0400

    update main file

--- main.c
+++ main.c
@@ -1,5 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "include/kash.h"
+
+pid_t shell_pgid;
+struct termios shell_tmodes;
+int shell_terminal;
+int shell_is_interactive;
+
+/* initialize the shell. */
+void init_shell()
+{
+    /* See if we are running interactively */
+    shell_terminal = STDIN_FILENO;
+    shell_is_interactive = isatty(shell_terminal);
+
+    if (shell_is_interactive)
+    {
+        /* Loop until we are in the foreground. */
+        while (tcgetpgrp(shell_terminal) != (shell_pgid = getpgrp()))
+            kill (- shell_pgid, SIGTTIN);
+
+        /* Ignore interactive and job-control signal */
+        signal(SIGINT,  SIG_IGN);
+        signal(SIGQUIT, SIG_IGN);
+        signal(SIGTSTP, SIG_IGN);
+        signal(SIGTTIN, SIG_IGN);
+        signal(SIGTTOU, SIG_IGN);
+        signal(SIGCHLD, SIG_IGN);
+
+        /* Put ourselves in our own process group. */
+        shell_pgid = getpid();
+        if (setpgid(shell_pgid, shell_pgid) < 0)
+        {
+            perror("Couldn't put the shell in its own process group");
+            exit(1);
+        }
+
+        /* Grab control of the terminal */
+        tcsetpgrp(shell_terminal, shell_pgid);
+
+        /* Save default terminal attributes for shell */
+        tcgetattr(shell_terminal, &shell_tmodes);
+    }
+}
 
 int main(int argc, char* argv[])
 {
