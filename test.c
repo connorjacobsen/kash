750f744e1ea60f2e33951660ba89633081a3ad5a
Connor Jacobsen <jacobsen.connor@gmail.com>
2015-03-16 23:12:55 -0400

    remove map

--- test.c
+++ test.c
@@ -5,31 +5,6 @@
 
 int tests_run = 0;
 
-static char *test_map()
-{
-    map_t* m = make_map();
-    mu_assert("Map initialized incorrectly", map_size(m) == 0);
-    map_put(m, "hello", "world");
-    mu_assert("Map put failed", map_size(m) == 1);
-
-    /* check contents of map */
-    mu_assert("Map contains failed", map_contains(m, "hello") == 1);
-    mu_assert("Map contains failed", map_contains(m, "foo") == 0);
-
-    mu_assert("Map get failed", map_get(m, "hello") == "world");
-
-    map_del(m, "hello");
-    mu_assert("Map contains failed -- post del", map_contains(m, "hello") == 0);
-    mu_assert("Map del incorrectly", map_size(m) == 0);
-    return 0;
-}
-
-static char *all_tests()
-{
-    mu_run_test(test_map);
-    return 0;
-}
-
 int main(int argc, char** argv)
 {
     char *result = all_tests();
